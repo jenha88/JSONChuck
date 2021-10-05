95952fd7d87c71380cd6e829fb95b054cdb0bdea
Jennifer Ha <Jennifer.Ha-1@ou.edu>
2021-10-05 16:15:08 -0500

    i tried

---
+++ json1/Chuck/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chuck
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            cmbBox.Items.Add("all");
+            cmbBox.SelectedIndex = 0;
+
+            using (var client = new HttpClient())
+            {
+                string jsonData = client.GetStringAsync("https://api.chucknorris.io/jokes/categories").Result;
+
+                List<string> Category = JsonConvert.DeserializeObject<List<string>>(jsonData);
+                foreach (var item in Category)
+                {
+                 
+                    cmbBox.Items.Add(item);
+                }
+            }
+        }
+
+        private void btb_Click(object sender, RoutedEventArgs e)
+        {
+            string s = cmbBox.SelectedItem.ToString();
+            using (var client= new HttpClient())
+            {
+                string jsonData = client.GetAsync("https://api.chucknorris.io/jokes/random?category=" +s);
+                
+            }
+        }
+        
+        private void cmbBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Category selecteditem = (Category)cmbBox.SelectedItem;
+
+        }
+    }
+}
